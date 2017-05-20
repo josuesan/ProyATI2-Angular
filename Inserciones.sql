3fae65ac00a1f1bf006f2f6bc8c15c2002c99a29
josuesan <josueguitar2@gmail.com>
2017-05-20 12:35:01 -0400

    Inclui las insercciones

---
+++ Inserciones.sql
@@ -0,0 +1,9 @@
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Camisa azul",150000,"http://tinyurl.com/l886g22","Es una camisa","camisas",11);
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Camisa Roja",66666,"http://tinyurl.com/mq39xcn","Es una camisa","camisas",12);
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Camisa de puntos",50000,"http://tinyurl.com/kz9m2ss","Es una camisa","camisas",14);
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Camisa Fea",45000,"http://tinyurl.com/n2xa4xo","Es una camisa","camisas",15);
+
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Pantalon",150000,"http://tinyurl.com/lqghlqw","Es un pantalon","pantalon",21);
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Pantalon Rojo",66666,"http://tinyurl.com/l3eu4z5","Es un pantalon","pantalon",22);
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Pantalon Marron",100000,"http://tinyurl.com/lrsdevp","Es un pantalon","pantalon",23);
+INSERT INTO `productos`(`nombre`, `precio`, `foto`, `descripcion`, `categoria`, `vendido`) VALUES ("Pantalon Deportivo",45000,"http://tinyurl.com/lf8d42x","Es un pantalon","pantalon",25);
